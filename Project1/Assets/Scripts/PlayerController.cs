40c854a61ca8b16ff5d456100ce625482a4de592
Jaysonzzr <jaysonzr2002@gmail.com>
2024-01-20 19:49:24 -0500

    Built the basic bedroom, and implemented the player with movement and camera

---
+++ Project1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public float gravity = 9.81f;
+
+    CharacterController controller;
+    Vector3 input, moveDirection;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        
+        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized * moveSpeed;
+
+        if (controller.isGrounded)
+        {
+            moveDirection = input;
+            moveDirection.y = 0;
+        }
+        else
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
