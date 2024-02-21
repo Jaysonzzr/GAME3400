c7d6211f4b3375f867b2af4a307a681e66881616
Jaysonzzr <jaysonzr2002@gmail.com>
2024-02-21 16:49:04 -0500

    Improved light interactions

--- Project5/Assets/Scripts/CameraInteractive.cs
+++ Project5/Assets/Scripts/CameraInteractive.cs
@@ -8,10 +8,17 @@ public class CameraInteractive : MonoBehaviour
     public float maxDistance; // Maximum distance at which object info will be displayed
 
     public Light spotlight;
+    public Light[] lights;
+
+    bool haveSpotLight = false;
+
+    public AudioClip collectSFX;
 
     // Update is called once per frame
     void Update()
     {
+        FlashLight();
+
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
@@ -26,13 +33,40 @@ public class CameraInteractive : MonoBehaviour
                     {
                         hitObject.GetComponent<MeshRenderer>().enabled = false;
                         hitObject.GetComponent<MeshCollider>().enabled = false;
-                        spotlight.enabled = true;
+                        AudioSource.PlayClipAtPoint(collectSFX, Camera.main.transform.position);
+                        haveSpotLight = true;
                     }
 
                     if (hitObject.CompareTag("PowerSwitch"))
                     {
                         RenderSettings.fog = false;
+                        
+                        foreach (Light light in lights)
+                        {
+                            light.enabled = true;
+                        }
+
+                        spotlight.enabled = false;
+                        haveSpotLight = false;
                     }
+                }
+            }
+        }
+    }
+
+    void FlashLight()
+    {
+        if (haveSpotLight)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (spotlight.enabled == true)
+                {
+                    spotlight.enabled = false;
+                }
+                else
+                {
+                    spotlight.enabled = true;
                 }
             }
         }
