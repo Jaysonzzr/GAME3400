1759b63bdb2e214099ccfdd00ce3931ebfba6ee9
Jaysonzzr <jaysonzr2002@gmail.com>
2024-04-11 15:15:54 -0400

    Implement mechanics

--- Final Project/Assets/Scripts/SafeBehavior.cs
+++ Final Project/Assets/Scripts/SafeBehavior.cs
@@ -10,11 +10,16 @@ public class SafeBehavior : MonoBehaviour
 
     bool triggered = false;
 
+    public Transform safeTrans;
+    public float rotationDuration = 2f;
+
     public void OpenSafe()
     {
         if (!triggered && audioSource != null && !audioSource.isPlaying && gameObject.GetComponent<AudioSource>() == null && !FindObjectOfType<TextDisplay>().isTextTyping)
         {
             FindObjectOfType<TextDisplay>().DisplayText(textToDisplay);
+
+            StartCoroutine(RotateSafeDoor());
             
             if (audioClip != null)
             {
@@ -32,5 +37,25 @@ public class SafeBehavior : MonoBehaviour
         yield return new WaitForSeconds(waitSecond);
         source.clip = clip;
         source.Play();
+    }
+
+    IEnumerator RotateSafeDoor()
+    {
+        float timeElapsed = 0;
+        float startAngle = 109f;
+        float endAngle = 230f;
+
+        while (timeElapsed < rotationDuration)
+        {
+            float zRotation = Mathf.Lerp(startAngle, endAngle, timeElapsed / rotationDuration);
+            
+            safeTrans.localRotation = Quaternion.Euler(safeTrans.localRotation.eulerAngles.x, safeTrans.localRotation.eulerAngles.y, zRotation);
+
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        safeTrans.localRotation = Quaternion.Euler(safeTrans.localRotation.eulerAngles.x, safeTrans.localRotation.eulerAngles.y, endAngle);
     }
 }
